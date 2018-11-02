cd7d49b2a6c5a9742b4b01125b5e7c7f4e466c0b
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2018-11-02 11:59:42 -0400

    metodo onclick e nome view

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ColorsActivity extends AppCompatActivity {
 
